f8980c807cdd7b878652c783f08b3350c91d25f0
EdFildes <43066140+EdFildes@users.noreply.github.com>
2024-03-08 11:20:26 +0000

    test url

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY . /usr/app
 RUN npm i
 RUN npx tsc 
 
+ENV NODE_ENV production
+
 CMD [ "node","./build/main.js" ] 
 
 EXPOSE 3000
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
 const makeMove = async (position) => {
-  const data = await fetch(`http://localhost:3000/move?position=${position}`).then(res => res.json());
+  const data = await fetch(`https://edfildes.co.uk/move?position=${position}`).then(res => res.json());
   document.getElementsByTagName("html")[0].innerHTML = data["data"]
 }
