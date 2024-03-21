0ef2c6a636d86d927dd7e3861ffecb496f64f89b
EdFildes <43066140+EdFildes@users.noreply.github.com>
2024-03-21 09:50:11 +0000

    added root url

--- src/main.ts
+++ src/main.ts
@@ -14,20 +14,21 @@ let game: GameInstance
 const title = "Go!!!"
 const message = "Lets play Go!"
 const size = 8
+const root_url = process.env.ROOT_URL
 
 var corsOptions = {
   origin: process.env.ORIGIN,
   optionsSuccessStatus: 200
 }
 
-app.get("/", cors(corsOptions), (req, res) => {
+app.get(`${root_url}/`, cors(corsOptions), (req, res) => {
   game = new Game(size);
   const positions = game.getPositions()
   res.render("index", { title, message, size: size, positions });
   console.log("\n !! NEW GAME !!\n")
 });
 
-app.get("/move", cors(corsOptions), (req, res) => {
+app.get(`${root_url}/move/`, cors(corsOptions), (req, res) => {
   if(!game){
     res.render("index", { title, message: "Please start a new game!", size: 0 });
   } else {
