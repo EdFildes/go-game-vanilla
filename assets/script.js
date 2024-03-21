24ac501dde623f29e8ae3ea7e3cd08120af056f9
EdFildes <43066140+EdFildes@users.noreply.github.com>
2024-03-21 11:39:02 +0000

    passed in path

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const makeMove = async (position) => {
+const makeMove = async (position, path) => {
   console.log("origin: ",origin)
   console.log("path: ",path)
   const data = await fetch(`${path}/move?position=${position}`).then(res => res.json());
