354cadf0658c427eee83c02f7bcbb3683be8d5dc
EdFildes <43066140+EdFildes@users.noreply.github.com>
2024-03-21 11:23:09 +0000

    debug

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 const makeMove = async (position) => {
+  console.log("origin: ",origin)
+  console.log("path: ",path)
   const data = await fetch(`${path}/move?position=${position}`).then(res => res.json());
   document.getElementsByTagName("html")[0].innerHTML = data["data"]
 }
--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,9 @@ const size = 8
 const origin = process.env.ORIGIN
 const root_url = process.env.ROOT_URL
 const path = `${origin}${root_url}`
+console.log("origin: ",origin)
+console.log("root_url: ",root_url)
+console.log("path: ",path)
 
 var corsOptions = {
   origin,
